cf9ded5dcc5d1aaab87f112408dd8721ca867dc1
garig0913 <90028734+garig0913@users.noreply.github.com>
2021-12-12 14:34:34 +0800

    commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,88 @@
+import { useState } from "react";
 import WebFont from "webfontloader";
-import { VscThreeBars } from 'react-icons/vsc'
+import { VscThreeBars } from "react-icons/vsc";
 
-const Navbar = props => {
-    WebFont.load({
-        google: {
-            families: ["Anton"],
-        },
-    });
-    return <>
-        <div className='w-full flex justify-between items-center py-4 fixed bg-transparent border-b border-gray-300'>
-            <div className='flex items-end ml-6 lg:ml-16'>
-                <VscThreeBars className='text-3xl text-green-900 mb-0.5 lg:hidden opacity-80 font-bold' />
-                <h1 className='text-4xl ml-3 text-green-900 font-extrabold'
-                    style={{
-                        font: 'Macan, Helvetica Neue, Helvetica, Arial, sans-serif',
-                        letterSpacing: '-2px'
-                    }}>fiverr<span className='text-green-400'>.</span></h1>
-            </div>
-            <div className='flex items-center gap-8 mr-20 text-lg' style={{
-                font: 'Macan, Helvetica Neue, Helvetica, Arial, sans-serif',
-                color: '#7a7d85'
-            }}>
-                <button id='transitionBtn' className='font-medium hover:text-green-500'>Fiverr Business</button>
-                <button id='transitionBtn' className='font-medium hover:text-green-500'>Explore</button>
-                <button id='transitionBtn' className='font-medium hover:text-green-500'>Become a seller</button>
-                <button id='transitionBtn' className='font-medium hover:text-green-500'>Become a seller</button>
-                <button id='transitionBtn' className='font-medium hover:text-green-500'>Sign in</button>
-                <button id='transitionBtn' className='font-medium hover:text-green-500' style={{ transition: '190ms' }} className='px-6 border border-gray-500 rounded
-                 mt-1 hover:bg-green-600 hover:text-gray-100 hover:border-gray-700'>Join</button>
-            </div>
-        </div>
-    </>
-}
+const Navbar = (props) => {
+   WebFont.load({
+      google: {
+         families: ["Anton"],
+      },
+   });
 
-export default Navbar
+   const [navbar, setNavbar] = useState(false);
+
+   const changeBg = () => {
+      if (window.scrollY > 50) {
+         setNavbar(true);
+      } else {
+         setNavbar(false);
+      }
+   };
+   window.addEventListener("scroll", changeBg);
+
+   return (
+      <>
+         <div
+            style={{
+               transition: "250ms",
+            }}
+            className={
+               navbar
+                  ? "w-full flex justify-between items-center py-4 fixed bg-blue-600 z-30"
+                  : "w-full flex justify-between items-center py-4 fixed bg-transparent z-30"
+            }
+         >
+            <div className="flex items-end ml-6 lg:ml-16">
+               <VscThreeBars className="text-3xl text-green-900 mb-0.5 lg:hidden opacity-80 font-bold" />
+               <h1
+                  className={
+                     navbar
+                        ? "text-4xl ml-3 text-gray-100 font-extrabold"
+                        : "text-4xl ml-3 text-green-900 font-extrabold"
+                  }
+                  style={{
+                     font: "Macan, Helvetica Neue, Helvetica, Arial, sans-serif",
+                     letterSpacing: "-2px",
+                  }}
+               >
+                  fiverr<span className="text-green-400">.</span>
+               </h1>
+            </div>
+            <div
+               className="flex items-center gap-8 mr-20"
+               style={{
+                  color: "#53635e",
+                  fontSize: "1.12rem",
+               }}
+            >
+               <button id="transitionBtn" className=" hover:text-green-500">
+                  Fiverr Business
+               </button>
+               <button id="transitionBtn" className=" hover:text-green-500">
+                  Explore
+               </button>
+               <button id="transitionBtn" className=" hover:text-green-500">
+                  Become a seller
+               </button>
+               <button id="transitionBtn" className=" hover:text-green-500">
+                  Become a seller
+               </button>
+               <button id="transitionBtn" className="hover:text-green-500">
+                  Sign in
+               </button>
+               <button
+                  id="transitionBtn"
+                  className="font-medium hover:text-green-500"
+                  style={{ transition: "190ms" }}
+                  className="px-6 border border-gray-500 rounded
+                 mt-1 hover:bg-green-600 hover:text-gray-100 hover:border-gray-700"
+               >
+                  Join
+               </button>
+            </div>
+         </div>
+      </>
+   );
+};
+
+export default Navbar;
---
+++ src/static/data/data.js
@@ -0,0 +1,53 @@
+export default const data = [
+    {
+       image: "https://dw3i9sxi97owk.cloudfront.net/homepageRevampA3/HeroImage/instance-1/hero-instance-1--desktop.webp",
+       color: "#1e0946",
+       skills: [
+          "web design",
+          "ux/ui design",
+          "android app development",
+          "software testing",
+       ],
+       placeholder: "web development",
+    },
+    {
+       image: "https://dw3i9sxi97owk.cloudfront.net/homepageRevampA3/HeroImage/instance-6/hero-instance-6--desktop.webp",
+       color: "#f68f84",
+       skills: ["graphic design", "3D logo", "branding", "illustration"],
+       placeholder: "graphic design",
+    },
+    {
+       image: "https://dw3i9sxi97owk.cloudfront.net/homepageRevampA3/HeroImage/instance-3/hero-instance-3--desktop.webp",
+       color: "#d9b100",
+       skills: [
+          "wordpress",
+          "php development",
+          "databases",
+          "software testing",
+       ],
+       placeholder: "advertising",
+    },
+    {
+       image: "https://dw3i9sxi97owk.cloudfront.net/homepageRevampA3/HeroImage/instance-5/hero-instance-5--desktop.webp",
+       color: "#91206b",
+       skills: [
+          "amazon store management",
+          "cms development",
+          "link building",
+          "adwords",
+       ],
+       placeholder: "e-commerce",
+    },
+    {
+       image: "https://dw3i9sxi97owk.cloudfront.net/homepageRevampA3/HeroImage/instance-4/hero-instance-4--desktop.webp",
+       color: "#e0221b",
+       skills: ["3D logo", "business cards", "branding", "vector logo"],
+       placeholder: "logo design",
+    },
+    {
+       image: "https://dw3i9sxi97owk.cloudfront.net/homepageRevampA3/HeroImage/instance-2/hero-instance-2--desktop.webp",
+       color: "#c23707",
+       skills: ["logo design", "illustration", "animation", "3D design"],
+       placeholder: "product design",
+    },
+ ];
