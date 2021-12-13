3616c4d62ab11efa8045efcea1ba84f5e38eada2
garig0913 <90028734+garig0913@users.noreply.github.com>
2021-12-13 13:57:30 +0800

    commit

--- src/components/Landing/Main.js
+++ src/components/Landing/Main.js
@@ -2,6 +2,7 @@ import Slider from "../Slider/Slider"
 import Slider2 from "../Slider/Slider2"
 import Slider3 from "../Slider/Slider3"
 import Navbar from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
 
 const Main = () => {
     const colors2 = [
@@ -29,7 +30,7 @@ const Main = () => {
             <img src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg.8b7310b.png' alt='abc' />
             <img src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal.ec56157.png' alt='abc' />
         </div>
-
+        <Footer />
     </>
 }
 
