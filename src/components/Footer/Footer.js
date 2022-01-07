976d12130f4d397d81afd38910e6c7d4aafcdd99
garig0913 <90028734+garig0913@users.noreply.github.com>
2022-01-07 14:29:21 +0800

    commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,13 @@ import { BsTwitter, BsFacebook, BsLinkedin, BsPinterest, BsInstagram } from "rea
 const Footer = () => {
   return (
     <>
-      <div className="py-8 w-full flex justify-center bg-gray-50 dark:text-white dark:bg-gray-800">
+      <div className="py-9 w-full flex justify-center bg-gray-50 dark:text-white dark:bg-gray-800">
         <div style={{ width: "90%" }} className="border-t flex justify-between py-5">
           {/* LEFT SIDE */}
           <div className="flex items-end">
             <h1
               id="logo"
-              className="text-4xl ml-3 font-extrabold text-gray-500"
+              className="text-4xl ml-3 font-extrabold text-gray-500 dark:text-gray-300"
               style={{
                 font: "Macan, Helvetica Neue, Helvetica, Arial, sans-serif",
                 letterSpacing: "-2px",
@@ -25,18 +25,18 @@ const Footer = () => {
                 ®
               </span>
             </h1>
-            <h1 className="mb-0.5 ml-6 text-sm font-semibold text-gray-400">
+            <h1 className="mb-0.5 ml-6 text-sm font-semibold text-gray-400 dark:text-gray-300">
               © Fiverr International Ltd. 2021
             </h1>
           </div>
 
           {/* RIGHT SIDE */}
-          <div className="flex gap-6 items-end text-gray-500 text-5xl">
-            <BsTwitter className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5" />
-            <BsFacebook className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5" />
-            <BsLinkedin className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5" />
-            <BsPinterest className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5" />
-            <BsInstagram className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5" />
+          <div className="flex gap-6 items-end text-gray-500 dark:text-gray-300 text-5xl">
+            <BsTwitter className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5 transition" />
+            <BsFacebook className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5 transition" />
+            <BsLinkedin className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5 transition" />
+            <BsPinterest className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5 transition" />
+            <BsInstagram className="hover:text-gray-700 cursor-pointer hover:bg-gray-200 rounded-xl p-2.5 transition" />
           </div>
         </div>
       </div>
--- src/components/Sidebar/Options.js
+++ src/components/Sidebar/Options.js
@@ -39,18 +39,22 @@ const Index = (props) => {
         }
         className="left-0 top-0 h-screen fixed z-50"
       >
-        <div className="w-full h-full pl-20 pr-4 flex flex-col relative bg-white border-r border-gray-600">
+        <div className="w-full h-full pl-20 pr-4 flex flex-col relative bg-white border-r border-gray-600 dark:bg-gray-800">
           <Btn
             onClick={OptionsHandler}
             text={sideBarHidden ? "options" : "close"}
             w={sideBarHidden ? "20" : "16"}
           />
 
-          <h1 className="text-lg font-semibold pb-3 mt-14 text-black">Filters</h1>
+          <h1 className="text-lg font-semibold pb-3 mt-14 text-black dark:text-gray-200">
+            Filters
+          </h1>
 
           <div className="flex flex-col">
             <div style={{ borderTop: "1px solid #aeb5bf" }} className="flex flex-col pb-6">
-              <h1 className="font-semibold text-gray-500 my-4">Freelancer Country</h1>
+              <h1 className="font-semibold text-gray-500 my-4 dark:text-gray-300">
+                Freelancer Country
+              </h1>
               <div
                 style={
                   countryFocus
@@ -60,21 +64,21 @@ const Index = (props) => {
                       }
                     : { transition: "170ms" }
                 }
-                className="flex items-center py-2 border border-gray-400 rounded"
+                className="flex items-center py-2 border border-gray-400 rounded dark:bg-gray-100"
               >
-                <BiSearch className="text-gray-500 mx-2 text-xl" />
+                <BiSearch className="text-gray-500 mx-2 text-xl dark:text-gray-300" />
                 <input
                   onFocus={CountryHandler}
                   onBlur={() => setCountryfocus(false)}
                   placeholder="enter a country"
-                  className="focus:outline-none"
+                  className="focus:outline-none dark:bg-gray-100"
                 />
               </div>
             </div>
             <form>
               <div style={{ borderTop: "1px solid #aeb5bf" }} className="flex flex-col pb-6">
                 <div className="flex items-center justify-between">
-                  <h1 className="font-semibold text-gray-800 my-4">Price</h1>
+                  <h1 className="font-semibold text-gray-800 my-4 dark:text-gray-300">Price</h1>
 
                   <IoIosArrowForward
                     style={
@@ -85,7 +89,7 @@ const Index = (props) => {
                           }
                         : { transition: "180ms" }
                     }
-                    className="text-xl text-gray-600"
+                    className="text-xl text-gray-600 dark:text-gray-300"
                     onClick={priceHandler}
                   />
                 </div>
@@ -94,43 +98,45 @@ const Index = (props) => {
                   style={{ transition: "0.28s", maxHeight: "1000px" }}
                   className={
                     priceVisibility
-                      ? "flex flex-col h-48 overflow-hidden"
-                      : "flex flex-col h-0 overflow-hidden"
+                      ? "flex flex-col h-0 overflow-hidden"
+                      : "flex flex-col h-48 overflow-hidden"
                   }
                 >
-                  <div className="flex text-gray-800 mb-1.5 ml-1">
+                  <div className="flex text-gray-800 mb-1.5 ml-1 dark:text-gray-300">
                     <input type="radio" className="mt-1 mr-4" name="prices" id="price1" />
-                    <label className="w-full" htmlFor="price1">
+                    <label className="w-full dark:bg-gray-600 pl-3 rounded-md" htmlFor="price1">
                       Under $20
                     </label>
                   </div>
-                  <div className="flex text-gray-800 mb-1.5 ml-1">
+                  <div className="flex text-gray-800 mb-1.5 ml-1 dark:text-gray-300">
                     <input type="radio" className="mt-1 mr-4" name="prices" id="price2" />
-                    <label className="w-full" htmlFor="price2">
+                    <label className="w-full dark:bg-gray-600 pl-3 rounded-md" htmlFor="price2">
                       $20 to $50
                     </label>
                   </div>
-                  <div className="flex text-gray-800 mb-1.5 ml-1">
+                  <div className="flex text-gray-800 mb-1.5 ml-1 dark:text-gray-300">
                     <input type="radio" className="mt-1 mr-4" name="prices" id="price3" />
-                    <label className="w-full" htmlFor="price3">
+                    <label className="w-full dark:bg-gray-600 pl-3 rounded-md" htmlFor="price3">
                       $50 to $100
                     </label>
                   </div>
-                  <div className="flex text-gray-800 mb-1.5 ml-1">
+                  <div className="flex text-gray-800 mb-1.5 ml-1 dark:text-gray-300">
                     <input type="radio" className="mt-1 mr-4" name="prices" id="price4" />
-                    <label className="w-full" htmlFor="price4">
+                    <label className="w-full dark:bg-gray-600 pl-3 rounded-md" htmlFor="price4">
                       Over $100
                     </label>
                   </div>
                   <div className="w-full flex justify-between h-10 mt-4">
                     <div style={{ width: "72%" }} className="h-full flex justify-between">
                       <input
+                        placeholder="min"
                         style={{ width: "47%" }}
-                        className="py-2.5 border border-gray-500 focus:outline-none pl-2 rounded"
+                        className="py-2.5 border border-gray-500 focus:outline-none pl-2 rounded dark:bg-gray-200"
                       />
                       <input
+                        placeholder="max"
                         style={{ width: "47%" }}
-                        className="py-2.5 border border-gray-500 focus:outline-none pl-2 rounded"
+                        className="py-2.5 border border-gray-500 focus:outline-none pl-2 rounded dark:bg-gray-200"
                       />
                     </div>
                     <div
@@ -148,13 +154,13 @@ const Index = (props) => {
             </form>
 
             <div style={{ borderTop: "1px solid #aeb5bf" }} className="flex flex-col pb-6">
-              <h1 className="font-semibold text-gray-800 my-4">More Filters</h1>
+              <h1 className="font-semibold text-gray-800 my-4 dark:text-gray-300">More Filters</h1>
               <div className="flex items-center gap-3">
                 <label class="switch">
                   <input type="checkbox" />
                   <span class="slider round"></span>
                 </label>
-                <h1 className="text-gray-500 mb-0.5">Recently added</h1>
+                <h1 className="text-gray-500 mb-0.5 dark:text-gray-300">Recently added</h1>
               </div>
             </div>
           </div>
